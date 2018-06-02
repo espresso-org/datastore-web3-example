841d0c88cda211e4aeaa7798eaaece582c477678
Justin Laroche <justinlaroche95@gmail.com>
2018-06-02 19:36:05 -0400

    Remove unused function

--- src/App.js
+++ src/App.js
@@ -115,28 +115,6 @@ function convertFileToArrayBuffer(file) {
 }
 
 
-function downloadFile(file: ArrayBuffer, filename: string) {
-  var blob = new Blob([file], { type: "application/pdf" })
-
-  // IE doesn't allow using a blob object directly as link href
-  // instead it is necessary to use msSaveOrOpenBlob
-  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-    window.navigator.msSaveOrOpenBlob(blob)
-    return
-  }
-
-  // For other browsers: 
-  // Create a link pointing to the ObjectURL containing the blob.
-  const data = window.URL.createObjectURL(blob)
-  var link = document.createElement('a')
-  link.href = data
-  link.download = filename
-  link.click()
-
-  // For Firefox it is necessary to delay revoking the ObjectURL
-  setTimeout(() => window.URL.revokeObjectURL(data), 100)
-
-}
 
 
 export default App
