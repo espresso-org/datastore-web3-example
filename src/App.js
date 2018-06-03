0a11c77c7649d4db6f30f7e2b931409922fee517
Justin Laroche <justinlaroche95@gmail.com>
2018-06-02 20:03:35 -0400

    Clean up uploadFiles function

--- src/App.js
+++ src/App.js
@@ -38,44 +38,14 @@ class App extends Component {
 
   }
 
-  uploadFiles = async () => {
-    var x = document.getElementById("myFile");
-    var txt = "";
-    if ('files' in x) {
-      if (x.files.length == 0) {
-        txt = "Select one or more files.";
-      } else {
-        for (var i = 0; i < x.files.length; i++) {
-          var file = x.files[i]
+  uploadFiles = async e => {
+    const files = e.target.files
 
-          const result = await convertFileToArrayBuffer(file)
+    for (let file of files) {
+      const result = await convertFileToArrayBuffer(file)      
+      await this.dataStore.addFile(result)        
+    }  
 
-          console.log('file array buffer: ', result)
-          //return this.ipfs.files.add(result)
-          const fileId = await this.dataStore.addFile(result)
-        
-          console.log('fileId: ', fileId)
-            
-
-          txt += "<br><strong>" + (i + 1) + ". file</strong><br>";
-          if ('name' in file) {
-            txt += "name: " + file.name + "<br>";
-          }
-          if ('size' in file) {
-            txt += "size: " + file.size + " bytes <br>";
-          }
-        }
-      }
-    }
-    else {
-      if (x.value == "") {
-        txt += "Select one or more files."
-      } else {
-        txt += "The files property is not supported by your browser!"
-        txt += "<br>The path of the selected file: " + x.value // If the browser does not support the files property, it will return the path of the selected file instead. 
-      }
-    }
-    document.getElementById("demo").innerHTML = txt
   }
 
   render() {
@@ -88,7 +58,7 @@ class App extends Component {
           {this.state.files.map((file, i) =>
             <div key={i}>{i}: {file.fileSize.toString()}</div>
           )}
-          <p id="demo"></p> 
+
 
         </main>
       </div>
