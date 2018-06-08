7a657e52a900359a756266516c1b5e4ab6760890
Justin Laroche <justinlaroche95@gmail.com>
2018-06-08 18:09:16 -0400

    Add action buttons to EditName component

--- src/components/edit-name.jsx
+++ src/components/edit-name.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 import { observer } from 'mobx-react'
 
@@ -10,12 +10,26 @@ const Main = styled.div`
     
 `
 
+
+
 export const EditName = observer(() =>
   <Main>
-      <Field label="Enter name here:">
-        <TextInput placeholder={mainStore.selectedFile.name} />
+      <Field label="New file name:">
+        <TextInput value={mainStore.selectedFile.name} />
       </Field>
+      <Actions>
+        <ActionButton mode="outline" onClick={() => /* TODO */0} emphasis="positive">OK</ActionButton>
+        <ActionButton mode="outline" onClick={() => mainStore.setEditMode(EditMode.None)} emphasis="negative">Cancel</ActionButton>
+      </Actions>
   </Main>
 )
 
+const Actions = styled.div`
+  margin-top: 40px;
+  margin-bottom: 20px;
+`
 
+const ActionButton = styled(Button)`
+  display: inline-block;
+  margin: 8px 10px;
+`
