241171e681975533f23b2dbd01bdb7e8ec0577cd
Mathew Cormier <mathew.corm@gmail.com>
2018-06-08 16:15:58 -0400

    Move logic to MainStore

--- src/App.js
+++ src/App.js
@@ -1,138 +1,71 @@
-import React, { Component } from 'react'
-import getWeb3 from './utils/getWeb3'
+import React from 'react'
 import styled from 'styled-components'
-import { AragonApp, AppBar, Button, Table, TableHeader, TableRow, TableCell, SidePanel } from '@aragon/ui'
+import { observer } from 'mobx-react'
+
+import { AragonApp, AppBar, Table, TableHeader, TableRow, SidePanel } from '@aragon/ui'
 
 import { AppLayout } from './components/app-layout'
 import { FileInput } from './components/file-input'
 import { FileRow } from './components/file-row'
 import { SideBar } from './components/side-bar'
 
-import { downloadFile, convertFileToArrayBuffer } from './utils/files'
 import { mainStore } from './stores/main-store'
-
-
-
-import { Datastore, providers } from 'datastore'
 
 import './App.css'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      selectedFile: null,
-      files: []
-    }
+const App = observer(() =>
+  <AragonApp publicUrl="/drive/">
+    <AppBar
+      title="Drive + web3"
+      endContent={
+        <FileInput onChange={mainStore.uploadFiles} >New File</FileInput>
+      }
+    />
+    <AppLayout.ScrollWrapper>
+      <AppLayout.Content>
+        <Breadcrumb>/</Breadcrumb>
+        <TwoPanels>
+          <Main>
+            <Table
+              header={
+                <TableRow>
+                  <TableHeader title="Name" />
+                  <TableHeader title="Owner" />
+                  <TableHeader title="Permissions" />
+                  <TableHeader title="Last Modified" />
+                </TableRow>
+              }
+            >
+              {mainStore.files.toJS().map(file => 
+                file && <FileRow
+                          key={file.id}
+                          file={file}
+                          selected={mainStore.isFileSelected(file)}
+                          onClick={() => mainStore.selectFile(file.id)}
+                        />
+              )}
+              
+            </Table>
+          </Main>
 
-  }
+          <SideBar file={mainStore.selectedFile} />
 
-  async componentWillMount() {
+        </TwoPanels>
+      </AppLayout.Content>
+    </AppLayout.ScrollWrapper>
 
-    try {
-      const results = await getWeb3
-
-      this.dataStore = new Datastore({
-        storageProvider: new providers.storage.Ipfs(),
-        encryptionProvider: new providers.encryption.Aes(),
-        rpcProvider: new providers.rpc.Web3(results.web3)
-      })
-
-      const files = await this.dataStore.listFiles()
-      this.setState({ files })
- 
-      window.dataStore = this.dataStore
-
-    } catch (e) {
-      console.log('Error ', e)
-    }
-
-
-  }
-
-  uploadFiles = async e => {
-    const files = e.target.files
-
-    for (let file of files) {      
-      const result = await convertFileToArrayBuffer(file)      
-      await this.dataStore.addFile(file.name, result)        
-    }  
-
-  }
-
-  downloadFile = async fileId => {
-    const file = await this.dataStore.getFile(fileId)
-    downloadFile(file.content, file.name)
-  }  
-
-  selectFile = async fileId => {
-    const selectedFile = this.state.files.filter(file => file.id === fileId)[0]
-    console.log('selectedFile: ', selectedFile)
-    if (selectedFile)
-      this.setState({ selectedFile })
-  }
-
-  isFileSelected = file => {
-    console.log('isFileSelected: ', file.id, this.state.selectedFile && this.state.selectedFile.id === file.id)
-    return this.state.selectedFile && this.state.selectedFile.id === file.id
-  }
-
-
-  render = () =>
-    <AragonApp publicUrl="/drive/">
-      <AppBar
-        title="Drive + web3"        
-        endContent={
-          <FileInput onChange={this.uploadFiles} >New File</FileInput>
-        }
-      />
-      <AppLayout.ScrollWrapper>
-        <AppLayout.Content>
-          <Breadcrumb>/</Breadcrumb>
-          <TwoPanels>
-
-            <Main>
-              <Table
-                header={
-                  <TableRow>
-                    <TableHeader title="Name" />
-                    <TableHeader title="Owner" />
-                    <TableHeader title="Permissions" />
-                    <TableHeader title="Last Modified" />
-                  </TableRow>
-                }
-              >
-                {this.state.files.map(file =>
-                  <FileRow 
-                    key={file.id} 
-                    file={file} 
-                    selected={this.isFileSelected(file)}
-                    onClick={() => this.selectFile(file.id)} 
-                  />
-                )}
-              </Table>
-            </Main>
-            
-            <SideBar file={this.state.selectedFile} /> 
-          
-          </TwoPanels>
-        </AppLayout.Content>
-      </AppLayout.ScrollWrapper>
-
-      <SidePanel
-        title="Change permissions"
-        opened={false}          
-      >
-      </SidePanel>
-    </AragonApp>
-
-}
+    <SidePanel
+      title="Change permissions"
+      opened={false}
+    >
+    </SidePanel>
+  </AragonApp>
+)
 
 const Breadcrumb = styled.div`
   font-size: 21px;
-  color: #000;
-`
+  color: #000;`
 
 const Main = styled.div`
   width: 100%;
@@ -142,10 +75,6 @@ const TwoPanels = styled.div`
   width: 100%;
   min-width: 800px;
 `
-
-
-
-
 
 
 export default App
--- src/stores/main-store.js
+++ src/stores/main-store.js
@@ -1,10 +1,65 @@
-import { observable } from 'mobx'
+import { observable, computed } from 'mobx'
+
+import { downloadFile, convertFileToArrayBuffer } from '../utils/files'
+import getWeb3 from '../utils/getWeb3'
+import { Datastore, providers } from 'datastore'
 
 
 class MainStore {
 
-    @observable files = []
+  @observable files = []
+  @observable selectedFile
+
+  isFileSelected(file) {
+    return this.selectedFile && this.selectedFile.id === file.id
+  }
+
+  uploadFiles = async e => {
+    const files = e.target.files
+
+    for (let file of files) {
+      const result = await convertFileToArrayBuffer(file)
+      await this.dataStore.addFile(file.name, result)
+    }
+
+  }
+
+  downloadFile = async fileId => {
+    const file = await this.dataStore.getFile(fileId)
+    downloadFile(file.content, file.name)
+  }
+
+  selectFile = async fileId => {
+    const selectedFile = mainStore.files.filter(file => file && file.id === fileId)[0]
+    console.log('selectedFile: ', selectedFile)
+    if (selectedFile)
+      mainStore.selectedFile = selectedFile
+  }
+
+  _datastore
+
+  constructor() {
+    this.initialize()
+    window.mainStore = this
+  }
+
+  async initialize() {
+    const results = await getWeb3
+
+    this._datastore = new Datastore({
+      storageProvider: new providers.storage.Ipfs(),
+      encryptionProvider: new providers.encryption.Aes(),
+      rpcProvider: new providers.rpc.Web3(results.web3)
+    })
+
+    this._refreshFiles()
+  }
+
+  async _refreshFiles() {
+    this.files = await this._datastore.listFiles()
+    console.log('files: ', this.files)
+  }
 
 }
 
-const mainStore = new MainStore()
+export const mainStore = new MainStore()
